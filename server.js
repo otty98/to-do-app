b9b669b02082268077b6be7c7844c1b69746263b
Owethu Peyane <opeyane98@gmail.com>
2025-08-18 19:13:10 +0200

    Mongo Atlas DB

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.static("public"));
